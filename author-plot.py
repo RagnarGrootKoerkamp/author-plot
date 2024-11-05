c9de78deb47b12c550b290b13173b55e82de3b0e
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-11-05 17:27:38 +0100

    Support loading order and tags from a yaml file

--- author-plot.py
+++ author-plot.py
@@ -22,6 +22,37 @@ if len(sys.argv) > 1:
 else:
     f = Path("references.bib")
 
+# An optional yaml file with structure:
+# - bibkey:
+#     tags: [list of tags]
+# that specifies the sort order.
+meta = f.with_suffix(".yaml")
+if meta.is_file():
+    # Read the YAML file.
+    import yaml
+
+    meta = yaml.safe_load(meta.open())
+
+    # Flatten the meta dictionary.
+    flat_meta = dict()
+    for i, kv in enumerate(meta):
+        assert len(kv) == 1
+        for k, v in kv.items():
+            if v is None:
+                v = dict()
+            if "tags" not in v:
+                v["tags"] = []
+            v["order"] = i
+            flat_meta[k] = v
+    meta = flat_meta
+
+else:
+    meta = None
+
+
+filter = "--filter" in sys.argv
+first = "--first" in sys.argv
+
 
 # Return list of [(key, year, month, title, [authors])]
 def parse(f):
@@ -60,66 +91,130 @@ def parse_tex(s):
 data = parse(f)
 data.sort(key=lambda x: x[1])
 
+if meta:
+    data.sort(key=lambda x: meta[x[0]]["order"])
+print(data)
+
 authors = []
+author_cnt = dict()
 new_data = []
+tags = []
+
 for key, year, month, title, ats in data:
     for a in ats:
         if a not in authors:
             authors.append(a)
-            # new_data.append((a, [a]))
-    new_data.append((key + " " + year, ats))
+            author_cnt[a] = 0
+        author_cnt[a] += 1
+
+    # First authors are always kept.
+    author_cnt[ats[0]] += 1
+
+    ts = meta[key]["tags"] if meta else []
+    new_data.append((key + " " + title + " " + year, ats, ts))
+
+    for t in ts:
+        if t not in tags:
+            tags.append(t)
+
+
+if filter:
+    authors = [a for a in authors if author_cnt[a] > 1]
+    for i in range(len(new_data)):
+        (val, ats, ts) = new_data[i]
+        new_data[i] = (val, [a for a in ats if author_cnt[a] > 1], ts)
 
 # dots
 data = new_data
 data.reverse()
-good = [
-    (j, i) for (i, (t, d)) in enumerate(data) for (j, a) in enumerate(authors) if a in d
-]
-bad = [
+A = len(authors) + 1
+all_dots = [(j, i) for i in range(len(data)) for j in range(len(authors))]
+all_dots += [(A + j, i) for i in range(len(data)) for j in range(len(tags))]
+author_dots = [
     (j, i)
-    for (i, (t, d)) in enumerate(data)
+    for (i, (_, d, _)) in enumerate(data)
     for (j, a) in enumerate(authors)
-    if a not in d
+    if a in d
 ]
+tag_dots = [
+    (A + j, i)
+    for (i, (_, _, ts)) in enumerate(data)
+    for (j, t) in enumerate(tags)
+    if t in ts
+]
+first_author_dots = [
+    (j, i)
+    for (i, (_, d, _)) in enumerate(data)
+    for (j, a) in enumerate(authors)
+    if a == d[0]
+]
+
 hlines = [
-    (min(authors.index(a) for a in d), max(authors.index(a) for a in d))
-    for (t, d) in data
+    (i, (min(authors.index(a) for a in d), max(authors.index(a) for a in d)))
+    for (i, (t, d, _)) in enumerate(data)
+]
+hlines += [
+    (i, (A + min(tags.index(t) for t in ts), A + max(tags.index(t) for t in ts)))
+    for (i, (_, _, ts)) in enumerate(data)
+    if ts
 ]
 
 scale = 1 / 4
 sz = 40
+first_sz = 12
 fig, ax = plt.subplots(figsize=(len(authors) * scale, len(data) * scale))
 
+# Background dots
 ax.scatter(
-    [x for (x, y) in good],
-    [y for (x, y) in good],
-    s=sz,
-    c="black",
-    zorder=10,
-)
-ax.scatter(
-    [x for (x, y) in bad],
-    [y for (x, y) in bad],
+    [x for (x, y) in all_dots],
+    [y for (x, y) in all_dots],
     s=sz,
     c="lightgrey",
     zorder=10,
 )
+# Horizontal paper connection lines
 ax.hlines(
-    list(range(len(data))),
-    [l for (l, r) in hlines],
-    [r for (l, r) in hlines],
+    [i for (i, _) in hlines],
+    [l for (_, (l, r)) in hlines],
+    [r for (_, (l, r)) in hlines],
     colors="black",
-    zorder=20,
+    zorder=10,
 )
+# Author dots
+ax.scatter(
+    [x for (x, y) in author_dots],
+    [y for (x, y) in author_dots],
+    s=sz,
+    c="black",
+    zorder=10,
+)
+# Tag dots
+ax.scatter(
+    [x for (x, y) in tag_dots],
+    [y for (x, y) in tag_dots],
+    s=sz,
+    c="black",
+    zorder=10,
+)
+# First author dots
+if first:
+    ax.scatter(
+        [x for (x, y) in first_author_dots],
+        [y for (x, y) in first_author_dots],
+        s=first_sz,
+        c="white",
+        zorder=10,
+    )
 ax.set_frame_on(False)
 ax.grid(False)
 ax.tick_params(axis="both", which="both", length=0)
 ax.yaxis.set_ticks(list(range(len(data))))
-ax.yaxis.set_ticklabels([t for (t, d) in data])
+ax.yaxis.set_ticklabels([t for (t, d, _) in data])
 ax.xaxis.set_ticks_position("top")
-ax.xaxis.set_ticks(list(range(len(authors))))
-ax.xaxis.set_ticklabels(authors, rotation=90)
+ax.xaxis.set_ticks(list(range(len(authors))) + list(range(A, A + len(tags))))
+ax.xaxis.set_ticklabels(authors + tags, rotation=90)
 
+# Grey rectangles
 for x in range(0, len(authors), 2):
     rect = plt.Rectangle(
         (x - 0.5, -0.5),
@@ -131,6 +226,20 @@ for x in range(0, len(authors), 2):
     )
 
     ax.add_patch(rect)
+
+for x in range(0, len(tags), 2):
+    x += A
+    rect = plt.Rectangle(
+        (x - 0.5, -0.5),
+        1,
+        len(data),
+        facecolor=(0.97, 0.97, 0.97),
+        edgecolor=None,
+        zorder=0,
+    )
+
+    ax.add_patch(rect)
+
 
 out = f.with_suffix(".svg")
 fig.savefig(out, bbox_inches="tight")
